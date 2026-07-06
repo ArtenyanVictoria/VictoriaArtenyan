3681fe157290b4f88d1a703d0184d07a64873594
ArtenyanVictoria <151942185+ArtenyanVictoria@users.noreply.github.com>
2026-07-06 13:32:57 +0300

    Сортировка проектов

--- Krestiki.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("schedule.json", "r") as file:
-    d = json.load(file)
-    for key_weeks in d:
-        for key_data in d[key_week]:
-            for x in d[key_week][key_data]:
-                for key_shedule in d[key_week][key_data][x]:
-                    
